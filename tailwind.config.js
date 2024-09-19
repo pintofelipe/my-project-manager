8d1b7652ea1e2f7110e9801acbe3503d596ebffb
juanmacar15 <111091727+juanmacar15@users.noreply.github.com>
2024-09-18 21:14:41 -0500

    add variables to colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'beige-1': '#F7EEDD' , // este es el color del background ----guiarse del figma
+      'beige-2': '#FFD299', // color para las cajas de los proyectos y los inputs
+      'beige-3':'#FAE7C3', // color para el background de las cajas que estan como protector del formulario y lor proyectos
+      'marron-1': '#8F1E00', // color para el texto del titulo del proyecto y los titulos de los botones
+      'marron-2': 'DD6236', // color para los botones de total y agregar proyecto
+      'gray-1': '#2C2C2C' // color para las descripciones y la prioridad
+
+    },
     extend: {},
   },
   plugins: [],
