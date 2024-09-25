8d9d99b6337ee7ef18a4c7872ae5e92fb792420f
pintofelipe <afpintou@ufpso.edu.co>
2024-09-25 13:02:25 -0500

    feat: add styles in component the login

--- src/components/LoginUser.jsx
+++ src/components/LoginUser.jsx
@@ -3,24 +3,28 @@ import React from 'react'
 
 const LoginUser = () =>{
     return(
-        <header className='bg-beige-2 grid grid-cols-2 h-96 content-center'>
-            <div className='bg-marron-2 h-72 content-center'>
+        <header className='bg-beige-2 grid grid-cols-2 h-[600px] content-center'>
+            <div className='bg-marron-2 h-[500px] flex justify-center items-center'>
+                <div className=''>
                 Logo
-                <h1>Sing Up Form</h1>
+                <h1 className='text-2xl font-semibold'>Sing Up Form</h1>
                 <h3>In React</h3>
+                </div>
             </div>
 
-            <div className='bg-marron-1 h-72'>
-                <h2>Sing Up</h2>
-                <input type="text" placeholder='Nombre'/>
-                <input type="email" placeholder='Email'/>
-                <input type="password" placeholder='Contraseña' />
+            <div className='bg-marron-1 h-[500px] px-10'>
+                <h2 className='mt-10 text-center text-1xl font-semibold'>Sing Up</h2>
+
+               <div className='bg-marron-2 grid-rows-4 grid mt-4'>
+                <input type="text" placeholder='Nombre' className='h-10 my-2 '/>
+                <input type="email" placeholder='Email' className='h-10 my-2'/>
+                <input type="password" placeholder='Contraseña' className='h-10 my-2'/>
                 <div>Olvidaste Contraseña? 
-                    <span className='text-marron-2'>Click Aquí</span>
+                    <span className='text-marron-1'>Click Aquí</span>
                 </div>
 
-                <div className='flex justify-center'>
-                    <button className='bg-marron-2 rounded-xl mx-3 px-10 h-8'>
+                <div className='flex justify-center mb-10'>
+                    <button className='bg-marron-1 rounded-xl mx-3 px-10 h-8'>
                         sing up
                     </button>
                     
@@ -28,6 +32,7 @@ const LoginUser = () =>{
                         sing in
                     </button>
                 </div>
+               </div>
 
             </div>
         </header>
